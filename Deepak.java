af3cd56341097e8b5190ef73878f3069ad8cf8b9
MysticAakash07 <rahulsagar460@gmail.com>
2023-05-30 21:53:52 +0530

    Editing index.html

---
+++ Deepak.java
@@ -0,0 +1,7 @@
+public class Deepak {
+    int a = 10;
+    public static void main(String[] args) {
+        Deepak d = new Deepak(); //this will generate a reference ID
+        System.out.println(d);
+    }
+}
